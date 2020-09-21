4ca5430b524463fac35774aa1d48eeedc17ed575
Fatih Kemal Akcay <fatihkemalakcay@gmail.com>
2020-09-21 17:50:45 +0100

    first commit

---
+++ src/main/java/collections/main.java
@@ -0,0 +1,12 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Queue<String> a = new LinkedList<String>();
+    }
+}
